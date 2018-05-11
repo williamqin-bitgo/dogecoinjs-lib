97f25ce91668418140b61f465ed3ddf9a187b3c9
Jorge Valdeiglesias <jorge@bitgo.com>
2018-05-11 16:04:10 -0700

    Add zcash to the network file

--- src/networks.js
+++ src/networks.js
@@ -2,6 +2,28 @@
 // Dogecoin BIP32 is a proposed standard: https://bitcointalk.org/index.php?topic=409731
 
 module.exports = {
+  zcash: {
+    messagePrefix: '\x18ZCash Signed Message:\n',
+    bech32: 'bc',
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4
+    },
+    pubKeyHash: 0x1cb8,
+    scriptHash: 0x1cbd,
+    wif: 0x80
+  },
+  zcashTest: {
+    messagePrefix: '\x18ZCash Signed Message:\n',
+    bech32: 'tb',
+    bip32: {
+      public: 0x043587cf,
+      private: 0x04358394
+    },
+    pubKeyHash: 0x1d25,
+    scriptHash: 0x1cba,
+    wif: 0xef
+  },
   bitcoingold: {
     messagePrefix: '\x18Bitcoin Gold Signed Message:\n',
     bip32: {
@@ -43,27 +65,5 @@ module.exports = {
     pubKeyHash: 0x30,
     scriptHash: 0x32,
     wif: 0xb0
-  },
-  zcash: {
-    messagePrefix: '\x18ZCash Signed Message:\n',
-    bech32: 'bc',
-    bip32: {
-      public: 0x0488b21e,
-      private: 0x0488ade4
-    },
-    pubKeyHash: 0x1cb8,
-    scriptHash: 0x1cbd,
-    wif: 0x80
-  },
-  zcashTest: {
-    messagePrefix: '\x18ZCash Signed Message:\n',
-    bech32: 'tb',
-    bip32: {
-      public: 0x043587cf,
-      private: 0x04358394
-    },
-    pubKeyHash: 0x1d25,
-    scriptHash: 0x1cba,
-    wif: 0xef
   }
 }
