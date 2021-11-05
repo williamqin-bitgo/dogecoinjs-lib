3c0020346ab60e00b539e7cc93d54316dc154f88
Brandon Black <brandonblack@bitgo.com>
2021-11-05 13:26:00 -0700

    fix(transaction): hashForWitnessV1 tests + fix ANYONECANPAY

--- src/transaction.js
+++ src/transaction.js
@@ -364,8 +364,8 @@ class Transaction {
       const input = this.ins[inIndex];
       sigMsgWriter.writeSlice(input.hash);
       sigMsgWriter.writeUInt32(input.index);
-      sigMsgWriter.writeVarSlice(prevOutScripts[inIndex]);
       sigMsgWriter.writeUInt64(values[inIndex]);
+      sigMsgWriter.writeVarSlice(prevOutScripts[inIndex]);
       sigMsgWriter.writeUInt32(input.sequence);
     } else {
       sigMsgWriter.writeUInt32(inIndex);
